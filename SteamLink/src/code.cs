c88f3f2542a4e8e1e01583828039b7f823d6c808
ADJStreams <adjstreams@gmail.com>
2023-10-09 02:46:09 +0100

    Fixed the logic

--- SteamLink/src/code.cs
+++ SteamLink/src/code.cs
@@ -3,132 +3,129 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
 public class CPHInline
 {
     private const string AUTH_ENDPOINT = "https://id.twitch.tv/oauth2/token";
     private const string IGDB_SEARCH_ENDPOINT = "https://api.igdb.com/v4/games";
     private const int STEAM_CATEGORY_ID = 1;
+    private const int TWITCH_CATEGORY_ID = 14;
     public bool Execute()
     {
-        string clientId = args["twitchClientId"]?.ToString();
-        string clientSecret = args["twitchClientSecret"]?.ToString();
-        if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(clientSecret))
+        try
         {
-            CPH.SendMessage("Please configure your Twitch Client ID and Client Secret for authentication before using this action");
-            return true;
-        }
-
-        string gameTitle = args["game"]?.ToString();
-        string gameId = args["gameId"]?.ToString();
-        if (string.IsNullOrEmpty(gameTitle))
-        {
-            CPH.SendMessage("Game category not set?");
-            return true;
-        }
-
-        if (gameTitle.ToLower() == "just chatting")
-        {
-            CPH.SendMessage("You're in the Just Chatting category!");
-            return true;
-        }
-
-        Task<KeyValuePair<string, string>?> fetchTask = FetchSteamUrlFromIGDB(clientId, clientSecret, gameTitle, gameId);
-        fetchTask.Wait();
-        var result = fetchTask.Result;
-        if (result != null)
-        {
-            if (result.Value.Key == gameTitle)
+            var configValid = ValidateConfiguration(out var clientId, out var clientSecret);
+            if (!configValid)
             {
-                CPH.SendMessage($"Steam URL for {result.Value.Key}: {result.Value.Value}");
+                return true;
             }
-            else
+
+            var gameValid = ValidateGame(out var gameTitle);
+            if (!gameValid)
             {
-                CPH.SendMessage($"Couldn't find {gameTitle}, but found {result.Value.Key} with this steam link: {result.Value.Value}");
+                return true;
             }
+
+            var fetchTask = FetchSteamUrlFromIGDB(clientId, clientSecret, gameTitle);
+            Task.Run(() => fetchTask).Wait();
+            HandleFetchResult(fetchTask.Result, gameTitle);
         }
-        else
+        catch (Exception ex)
         {
-            CPH.SendMessage($"Steam URL not found for {gameTitle}.");
+            CPH.LogError($"There was an error: {ex}");
+            CPH.SendMessage("Sorry, try again later, there's an issue getting the steam link");
         }
 
         return true;
     }
 
-    static async Task<KeyValuePair<string, string>?> FetchSteamUrlFromIGDB(string clientId, string clientSecret, string gameTitle, string gameId)
+    private bool ValidateConfiguration(out string clientId, out string clientSecret)
     {
-        using (HttpClient client = new HttpClient())
+        clientId = args["twitchClientId"]?.ToString();
+        clientSecret = args["twitchClientSecret"]?.ToString();
+        if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(clientSecret))
         {
-            string authEndpoint = $"{AUTH_ENDPOINT}?client_id={clientId}&client_secret={clientSecret}&grant_type=client_credentials";
-            HttpResponseMessage authResponse = await client.PostAsync(authEndpoint, null).ConfigureAwait(false);
-            string authResult = await authResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-            string accessToken = JObject.Parse(authResult)["access_token"].ToString();
+            CPH.SendMessage("Please configure your Twitch Client ID and Client Secret.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool ValidateGame(out string gameTitle)
+    {
+        gameTitle = args["game"]?.ToString();
+        if (string.IsNullOrEmpty(gameTitle))
+        {
+            CPH.SendMessage("Game category not set?");
+            return false;
+        }
+
+        if (gameTitle.ToLower() == "just chatting")
+        {
+            CPH.SendMessage("You're in the Just Chatting category!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private void HandleFetchResult(KeyValuePair<string, string>? result, string gameTitle)
+    {
+        if (result != null)
+        {
+            if (result.Value.Key == gameTitle)
+            {
+                CPH.SendMessage($"Steam URL for {result.Value.Key}: {result.Value.Value}");
+                return;
+            }
+        }
+
+        CPH.SendMessage($"Steam URL not found for {gameTitle}.");
+        return;
+    }
+
+    private static async Task<KeyValuePair<string, string>?> FetchSteamUrlFromIGDB(string clientId, string clientSecret, string gameTitle)
+    {
+        using (var client = new HttpClient())
+        {
+            var accessToken = await GetAccessToken(client, clientId, clientSecret);
             client.DefaultRequestHeaders.Add("Client-ID", clientId);
             client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
-            string queryFields = "fields external_games.url, external_games.uid, name; where external_games.category = 1;";
-            // Try fetching by gameId
-            if (!string.IsNullOrEmpty(gameId))
-            {
-                var idPayload = new StringContent($"{queryFields} where external_games.uid = \"{gameId}\";", System.Text.Encoding.UTF8, "text/plain");
-                var idResponse = await FetchUrlFromIGDBHelper(client, idPayload).ConfigureAwait(false);
-                if (idResponse != null)
-                {
-                    return idResponse;
-                }
-            }
-
-            // Try fetching by gameTitle
-            var titlePayload = new StringContent($"{queryFields} search \"{gameTitle}\";", System.Text.Encoding.UTF8, "text/plain");
-            return await FetchUrlFromIGDBHelper(client, titlePayload).ConfigureAwait(false);
+            var payload = new StringContent($"fields external_games.url, external_games.category, name; where name ~ *\"{gameTitle}\"* & external_games.category = {STEAM_CATEGORY_ID};", System.Text.Encoding.UTF8, "text/plain");
+            return await FetchUrlFromIGDBHelper(client, payload);
         }
     }
 
-    static async Task<KeyValuePair<string, string>?> FetchUrlFromIGDBHelper(HttpClient client, StringContent payload)
+    private static async Task<string> GetAccessToken(HttpClient client, string clientId, string clientSecret)
     {
-        HttpResponseMessage response = await client.PostAsync(IGDB_SEARCH_ENDPOINT, payload).ConfigureAwait(false);
-        if (response == null || response.Content == null)
-        {
-            return null;
-        }
+        var authEndpoint = $"{AUTH_ENDPOINT}?client_id={clientId}&client_secret={clientSecret}&grant_type=client_credentials";
+        var authResponse = await client.PostAsync(authEndpoint, null).ConfigureAwait(false);
+        var authResult = await authResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return JObject.Parse(authResult)["access_token"].ToString();
+    }
 
-        string result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+    private static async Task<KeyValuePair<string, string>?> FetchUrlFromIGDBHelper(HttpClient client, StringContent payload)
+    {
+        var response = await client.PostAsync(IGDB_SEARCH_ENDPOINT, payload).ConfigureAwait(false);
+        var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
         if (string.IsNullOrEmpty(result))
         {
             return null;
         }
 
-        JArray games = JArray.Parse(result);
-        if (games == null || games.Count == 0)
+        var games = JArray.Parse(result);
+        var firstGame = (JObject)games.First;
+        var foundGameTitle = firstGame["name"]?.ToString();
+        var externalGames = (JArray)firstGame["external_games"];
+        var filteredGame = externalGames.FirstOrDefault(eg => eg["category"]?.Value<int>() == STEAM_CATEGORY_ID);
+        if (filteredGame == null)
         {
             return null;
         }
 
-        foreach (var game in games)
-        {
-            string foundGameTitle = game["name"]?.ToString();
-            var externalGame = game["external_games"]?.First;
-            if (externalGame == null)
-            {
-                return null;
-            }
-
-            string steamUrl = externalGame["url"]?.ToString();
-            if (!string.IsNullOrEmpty(steamUrl))
-            {
-                return new KeyValuePair<string, string>(foundGameTitle, steamUrl);
-            }
-            else
-            {
-                // Construct Steam URL from UID
-                string uid = externalGame["uid"]?.ToString();
-                if (!string.IsNullOrEmpty(uid))
-                {
-                    steamUrl = $"https://store.steampowered.com/app/{uid}";
-                    return new KeyValuePair<string, string>(foundGameTitle, steamUrl);
-                }
-            }
-        }
-
-        return null;
+        var steamUrl = filteredGame["url"]?.ToString();
+        return !string.IsNullOrEmpty(steamUrl) ? new KeyValuePair<string, string>(foundGameTitle, steamUrl) : null;
     }
 }
